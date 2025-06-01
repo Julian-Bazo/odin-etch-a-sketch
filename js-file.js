d4244d52424910869e20b9f44997153a1c017aa5
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-06-01 17:56:52 -0400

    Add customize button functionality
    Allows for the creation of a new grid with dimensions input by the user.

--- js-file.js
+++ js-file.js
@@ -10,6 +10,14 @@ customizeGridButton.textContent = "Click me to customize the grid";
 customizeGridButton.classList.add("customizeButton");
 gridContainer.appendChild(customizeGridButton);
 
+// Add button on-click event listener
+customizeGridButton.addEventListener("click", () => {
+    let totalGrid = prompt("Choose new grid size", "");
+    gridLength = totalGrid;
+    gridHeight = totalGrid;
+    createGrid(gridHeight, gridLength);
+});
+
 // Grid length and height starting variables
 let gridLength = 16;
 let gridHeight = 16;
@@ -17,20 +25,22 @@ let gridHeight = 16;
 // Loop to create grid
 
 // This first loop creates the number of vertical rows
-for (i = 0; i < gridHeight; i++) {
-    const newYDiv = document.createElement('div');
-    newYDiv.classList.add(`yDiv`);
-    gridContainer.appendChild(newYDiv);
+function createGrid(gridHeight, gridLength){
+    for (i = 0; i < gridHeight; i++) {
+        const newYDiv = document.createElement('div');
+        newYDiv.classList.add(`yDiv`);
+        gridContainer.appendChild(newYDiv);
 
     // This second loop creates the number of horizontal columns
-    for (j = 0; j < gridLength; j++) { 
-        const newXDiv = document.createElement("div");
-        newXDiv.classList.add("drawDiv");
-        // newXDiv.textContent = `Div ${j +1}`;
-        newYDiv.appendChild(newXDiv);
+        for (j = 0; j < gridLength; j++) { 
+            const newXDiv = document.createElement("div");
+            newXDiv.classList.add("drawDiv");
+            newYDiv.appendChild(newXDiv);
     }
 }
+};
 
+createGrid(gridHeight, gridLength);
 
 // Array.apply(null, gridArray(gridLength));
 
