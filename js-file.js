e6d9ec47c48ada849081487f37f6885c49beb5bb
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-06-01 17:34:20 -0400

    Fix grid creation loop
    The index variable for the horizontal div loop was switched from i to j.

--- js-file.js
+++ js-file.js
@@ -2,16 +2,16 @@
 const gridContainer = document.querySelector(".container");
 
 let gridLength = 16;
-let gridHeight = 1;
+let gridHeight = 16;
 
 for (i = 0; i < gridHeight; i++) {
     const newYDiv = document.createElement('div');
     newYDiv.classList.add(`yDiv`);
     gridContainer.appendChild(newYDiv);
-    for (i = 0; i < gridLength; i++) { 
+    for (j = 0; j < gridLength; j++) { 
         const newXDiv = document.createElement("div");
         newXDiv.classList.add("drawDiv");
-        newXDiv.textContent = `Div ${i +1}`;
+        newXDiv.textContent = `Div ${j +1}`;
         newYDiv.appendChild(newXDiv);
     }
 }
