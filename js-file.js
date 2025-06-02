8bf638dbb60f3c42a99089da0807fe36b0d08466
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-06-02 09:20:37 -0400

    Fix hover effect to be permanent
    Fixes the hover effect over the individual divs to make the "drawing" permanent when a "mouseover" event listener is activated.
    
    Some DOM restructuring added to create a div specifically for the grid separate from the recreate button.

--- js-file.js
+++ js-file.js
@@ -1,46 +1,69 @@
 // Initialize DOM elements
 
-// Grid
-const gridContainer = document.querySelector(".container");
-gridContainer.classList.add("container");
+// Starting div to hold button and grid div
+const totalContainer = document.querySelector(".totalContainer");
+totalContainer.classList.add("totalContainer");
 
 // Customization button
 const customizeGridButton = document.createElement('button');
 customizeGridButton.textContent = "Click me to customize the grid";
 customizeGridButton.classList.add("customizeButton");
-gridContainer.appendChild(customizeGridButton);
+totalContainer.appendChild(customizeGridButton);
+
+// Grid div
+const gridDiv = document.createElement("div");
+gridDiv.classList.add("gridDiv");
+totalContainer.appendChild(gridDiv);
 
 // Add button on-click event listener
 customizeGridButton.addEventListener("click", () => {
-    let totalGrid = prompt("Choose new grid size", "");
-    gridLength = totalGrid;
-    gridHeight = totalGrid;
+    let gridSize = prompt("Choose new grid size", "");
+    globalDeleteChecker++;
+    gridLength = gridSize;
+    gridHeight = gridSize;
     createGrid(gridHeight, gridLength);
+    // console.log(globalDeleteChecker);
 });
 
 // Grid length and height starting variables
 let gridLength = 16;
 let gridHeight = 16;
 
+// Global variable to allow grid deletion
+let globalDeleteChecker = 0;
+
 // Loop to create grid
 
 // This first loop creates the number of vertical rows
 function createGrid(gridHeight, gridLength){
-    for (i = 0; i < gridHeight; i++) {
-        const newYDiv = document.createElement('div');
-        newYDiv.classList.add(`yDiv`);
-        gridContainer.appendChild(newYDiv);
+    
+    const tempGrid = document.createElement('div');
+    tempGrid.classList.add("tempGrid");
+    gridDiv.appendChild(tempGrid);
 
-    // This second loop creates the number of horizontal columns
-        for (j = 0; j < gridLength; j++) { 
-            const newXDiv = document.createElement("div");
-            newXDiv.classList.add("drawDiv");
-            newYDiv.appendChild(newXDiv);
+        for (i = 0; i < gridHeight; i++) {
+            const newYDiv = document.createElement('div');
+            newYDiv.classList.add("yDiv");
+            tempGrid.appendChild(newYDiv);
+
+        // This second loop creates the number of horizontal columns
+            for (j = 0; j < gridLength; j++) { 
+                const newXDiv = document.createElement("div");
+                newXDiv.classList.add("drawDiv");
+                newXDiv.addEventListener("mouseenter", () => {
+                    newXDiv.classList.add("drawnDiv");
+                })
+                newYDiv.appendChild(newXDiv);
+        }
+        }
+
+    if (globalDeleteChecker > 0) {
+        
     }
-}
 };
 
 createGrid(gridHeight, gridLength);
+
 
 // Array.apply(null, gridArray(gridLength));
 
